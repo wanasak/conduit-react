d991b38b0322003ceb2d944838952aa88869033c
Wanasak Suraintaranggoon <u510610433@gmail.com>
2019-05-29 16:58:10 +0700

    apply tag

--- src/reducers/articleList.js
+++ src/reducers/articleList.js
@@ -1,17 +1,46 @@
 import {
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
-  SET_PAGE
+  SET_PAGE,
+  APPLY_TAG_FILTER,
+  CHANGE_TAB,
+  ARTICLE_FAVORITED,
+  ARTICLE_UNFAVORITED
 } from '../constants/actionTypes';
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case ARTICLE_FAVORITED:
+    case ARTICLE_UNFAVORITED:
+      return {
+        ...state,
+        articles: state.articles.map(article => {
+          if (article.slug === action.payload.article.slug) {
+            return {
+              ...article,
+              favorited: action.payload.article.favorited,
+              favoritesCount: action.payload.article.favoritesCount
+            };
+          }
+          return article;
+        })
+      };
     case SET_PAGE:
       return {
         ...state,
         articles: action.payload.articles,
         articlesCount: action.payload.articlesCount,
         currentPage: action.page
+      };
+    case APPLY_TAG_FILTER:
+      return {
+        ...state,
+        pager: action.pager,
+        articles: action.payload.articles,
+        articlesCount: action.payload.articlesCount,
+        tab: null,
+        tag: action.tag,
+        currentPage: 0
       };
     case HOME_PAGE_LOADED:
       return {
@@ -25,6 +54,16 @@ export default (state = {}, action) => {
       };
     case HOME_PAGE_UNLOADED:
       return {};
+    case CHANGE_TAB:
+      return {
+        ...state,
+        pager: action.pager,
+        articles: action.payload.articles,
+        articlesCount: action.payload.articlesCount,
+        tab: action.tab,
+        currentPage: 0,
+        tag: null
+      };
     default:
       return state;
   }
