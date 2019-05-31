93dbdb2958c98f79963070e5b5399b8ac1612c71
Wanasak <u510610433@gmail.com>
2019-05-31 22:07:04 +0700

    settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,14 @@
-import React, { Component } from 'react';
+import ListErrors from './ListErrors';
+import React from 'react';
+import agent from '../agent';
 import { connect } from 'react-redux';
+import {
+  SETTINGS_SAVED,
+  SETTINGS_PAGE_UNLOADED,
+  LOGOUT
+} from '../constants/actionTypes';
 
-class SettingsForm extends Component {
+class SettingsForm extends React.Component {
   constructor() {
     super();
 
@@ -12,55 +19,107 @@ class SettingsForm extends Component {
       email: '',
       password: ''
     };
+
+    this.updateState = field => ev => {
+      const state = this.state;
+      const newState = Object.assign({}, state, { [field]: ev.target.value });
+      this.setState(newState);
+    };
+
+    this.submitForm = ev => {
+      ev.preventDefault();
+
+      const user = Object.assign({}, this.state);
+      if (!user.password) {
+        delete user.password;
+      }
+
+      this.props.onSubmitForm(user);
+    };
   }
 
-  componentWillMount() {}
+  componentWillMount() {
+    if (this.props.currentUser) {
+      Object.assign(this.state, {
+        image: this.props.currentUser.image || '',
+        username: this.props.currentUser.username,
+        bio: this.props.currentUser.bio || '',
+        email: this.props.currentUser.email
+      });
+    }
+  }
 
-  componentWillReceiveProps(nextProps) {}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.currentUser) {
+      this.setState(
+        Object.assign({}, this.state, {
+          image: nextProps.currentUser.image || '',
+          username: nextProps.currentUser.username,
+          bio: nextProps.currentUser.bio || '',
+          email: nextProps.currentUser.email
+        })
+      );
+    }
+  }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.submitForm}>
         <fieldset>
           <fieldset className="form-group">
             <input
-              type="text"
               className="form-control"
+              type="text"
               placeholder="URL of profile picture"
+              value={this.state.image}
+              onChange={this.updateState('image')}
             />
           </fieldset>
 
           <fieldset className="form-group">
             <input
+              className="form-control form-control-lg"
               type="text"
-              className="form-control"
               placeholder="Username"
+              value={this.state.username}
+              onChange={this.updateState('username')}
             />
           </fieldset>
 
           <fieldset className="form-group">
             <textarea
-              className="form-control"
+              className="form-control form-control-lg"
               rows="8"
               placeholder="Short bio about you"
+              value={this.state.bio}
+              onChange={this.updateState('bio')}
             />
-          </fieldset>
-
-          <fieldset className="form-group">
-            <input type="email" className="form-control" placeholder="Email" />
           </fieldset>
 
           <fieldset className="form-group">
             <input
+              className="form-control form-control-lg"
+              type="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.updateState('email')}
+            />
+          </fieldset>
+
+          <fieldset className="form-group">
+            <input
+              className="form-control form-control-lg"
               type="password"
-              className="form-control"
               placeholder="New Password"
+              value={this.state.password}
+              onChange={this.updateState('password')}
             />
           </fieldset>
 
           <button
-            type="submit"
             className="btn btn-lg btn-primary pull-xs-right"
+            type="submit"
+            disabled={this.state.inProgress}
           >
             Update Settings
           </button>
@@ -75,11 +134,42 @@ const mapStateToProps = state => ({
   currentUser: state.common.currentUser
 });
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  onClickLogout: () => dispatch({ type: LOGOUT }),
+  onSubmitForm: user =>
+    dispatch({ type: SETTINGS_SAVED, payload: agent.Auth.save(user) }),
+  onUnload: () => dispatch({ type: SETTINGS_PAGE_UNLOADED })
+});
 
-class Settings extends Component {
+class Settings extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div className="settings-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Your Settings</h1>
+
+              <ListErrors errors={this.props.errors} />
+
+              <SettingsForm
+                currentUser={this.props.currentUser}
+                onSubmitForm={this.props.onSubmitForm}
+              />
+
+              <hr />
+
+              <button
+                className="btn btn-outline-danger"
+                onClick={this.props.onClickLogout}
+              >
+                Or click here to logout.
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/reducers/common.js
+++ src/reducers/common.js
@@ -8,7 +8,12 @@ import {
   HOME_PAGE_UNLOADED,
   LOGOUT,
   REDIRECT,
-  ARTICLE_SUBMITTED
+  ARTICLE_SUBMITTED,
+  SETTINGS_SAVED,
+  EDITOR_PAGE_UNLOADED,
+  PROFILE_PAGE_UNLOADED,
+  SETTINGS_PAGE_UNLOADED,
+  PROFILE_FAVORITES_PAGE_UNLOADED
 } from '../constants/actionTypes';
 
 const defaultState = {
@@ -30,6 +35,15 @@ export default (state = defaultState, action) => {
       return { ...state, redirectTo: null };
     case LOGOUT:
       return { ...state, redirectTo: '/', token: null, currentUser: null };
+    case ARTICLE_SUBMITTED:
+      const redirectTo = `/article/${action.payload.article.slug}`;
+      return { ...state, redirectTo };
+    case SETTINGS_SAVED:
+      return {
+        ...state,
+        redirectTo: action.error ? null : '/',
+        currentUser: action.error ? null : action.payload.user
+      };
     case LOGIN:
     case REGISTER:
       return {
@@ -39,13 +53,14 @@ export default (state = defaultState, action) => {
         currentUser: action.error ? null : action.payload.user
       };
     case ARTICLE_PAGE_UNLOADED:
+    case EDITOR_PAGE_UNLOADED:
     case HOME_PAGE_UNLOADED:
+    case PROFILE_PAGE_UNLOADED:
+    case PROFILE_FAVORITES_PAGE_UNLOADED:
+    case SETTINGS_PAGE_UNLOADED:
     case LOGIN_PAGE_UNLOADED:
     case REGISTER_PAGE_UNLOADED:
       return { ...state, viewChangeCounter: state.viewChangeCounter + 1 };
-    case ARTICLE_SUBMITTED:
-      const redirectTo = `/article/${action.payload.article.slug}`;
-      return { ...state, redirectTo };
     default:
       return state;
   }
